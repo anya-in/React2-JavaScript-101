178b44d6c00ae8f1db024c825b4aa137db42b218
Lavanya Lahari <lavanyalaharikalla@gmail.com>
2022-06-24 23:36:20 +0530

    init

---
+++ Javascript101/main.js
@@ -0,0 +1,154 @@
+//console.log('Hello World');
+//console.error('This is an error');
+
+/*const name = 'Laly';
+const age = 30;
+const rating = 4.5;
+const iscool = true;
+const x = null;
+const y = undefined;
+let z;*/
+
+//console.log(typeof x);
+//concatenation
+//console.log('My name is ' + name + ' and i am of age ' + age);
+//const hello = `My name is ${name} and Iam of age ${age}`;
+//console.log(hello);
+
+//Functions
+//const s = 'Hello World';
+//console.log(s.length);
+//console.log(s.substring(0, 5));
+//Arrays
+//const fruits = ['apples, 'oranges', 'pears'];
+//fruits.push('mangoes');
+//fruits.pop();
+//console.log(fruits.indexOf('oranges));
+//console.log(fruits);
+
+//Object pairs
+
+/*const person = {
+    firstName: 'Lavanya',
+    lastName: 'Kalla',
+    age: '30',
+    hobbies: ['movies', 'Music', 'Reading']
+}
+console.log(person);
+//console.log(person.firstName);
+*/
+
+/*person.email = 'xyz.com';
+const todos = [
+    {
+        id: 1,
+        text: 'finish the tutorial',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Publish It ',
+        isCompleted: false
+    },
+    {
+        id: 3,
+        text: 'Update the Sheet ',
+        isCompleted: false
+    }
+];*/
+
+//console.log(todos[1].text);
+//JSON
+
+//const todoJSON = JSON.stringify(todos);
+
+//for
+/*for (let i = 0; i < 10; i++) {
+
+}*/
+
+
+//while
+/*let i = 0;
+while (i < 10) {
+
+}*/
+
+//Loop through arrays
+/*for (let i = 0; i < todos.length; i++) {
+    console.log(todos[i].text);
+
+}*/
+
+/*for (let todo of todos) {
+    console.log(todo.text);
+}*/
+
+//for Each, Map, Filter
+
+/*todos.forEach(function (todo) {
+    console.log(todo.text);
+});*/
+
+/*const todoText = todos.map(function (todo) {
+    return todo.text;
+});
+
+//console.log(todoText);
+
+const todoCompleted = todos.filter(function (todo) {
+    return todo.isCompleted === true;
+});
+*/
+//console.log(todoCompleted);
+
+
+//Conditionals
+// const x = 10;
+// if (x == 10) {
+//     console.log('x is 10');
+// } else {
+//     console.log('x is not 10');
+// }
+
+
+
+// const x = 11;
+// const color = x > 10 ? 'red' : 'blue';
+// console.log(color);
+// switch (color) {
+//     case 'red':
+//         console.log('color is red');
+//         break;
+//     case 'blue':
+//         console.log('color is blue');
+// }
+
+//Functions
+
+// function addNums(num1, num2) {
+//     console.log('num1+num2');
+// }
+
+// addNums(1, 1);
+
+//Arrow function
+//const addNums = (num1, num2) => num1 + num2;
+//console.log(addNums(5, 5));
+
+//Constructor
+
+// function Person(firstName, lastName, dob) {
+//     this.firstname = firstName;
+//     this.lastname = lastName;
+//     this.dob = new Date(dob);
+
+// }
+
+// //Instantiate
+// const person1 = new Person('lavanya', 'k', '4-16-1991');
+
+// console.log(person1.dob);
+
+
+
